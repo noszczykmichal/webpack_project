f597b8d2ff808bff8df3f144649b30c3f90b9917
Michał Noszczyk <noszczykmichal@wp.pl>
2021-04-10 20:57:17 +0200

    added two components and containers, hoc and webpack config

---
+++ src/components/PizzaImage/PizzaImage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './PizzaImage.css';
+import PizzaImage from '../../assets/pizza.jpg';
+
+
+const pizzaImage = (props) => (
+    <div className={classes.PizzaImage}>
+        <img src={PizzaImage} className={classes.PizzaPict}/>
+    </div>
+);
+
+export default pizzaImage;
